890e322d013678df5776d4a9d2c0f42b9c8dadeb
Megan Miller <megan.l.miller264@gmail.com>
2022-06-16 11:14:14 -0400

    Initial commit

---
+++ src/Settings/SkeletonSettings.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace VendorName\Skeleton\Settings;
+
+use Helix\Lego\Settings\AppSettings;
+use VendorName\Skeleton\Actions\SkeletonAction;
+
+class SkeletonSettings extends AppSettings
+{
+    // public string $url;
+
+    public function rules(): array
+    {
+        return [
+            // 'url' => Rule::requiredIf($this->enabled === true),
+        ];
+    }
+
+    // protected static array $actions = [
+    //     SkeletonAction::class,
+    // ];
+
+    // public static function encrypted(): array
+    // {
+    //     return ['access_token'];
+    // }
+
+    public function description(): string
+    {
+        return 'Interact with Skeleton.';
+    }
+
+    public static function group(): string
+    {
+        return 'skeleton';
+    }
+}
